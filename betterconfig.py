aff68a70c5dd5fa9b5692906154b6bb9a43fab81
Matthew Story <matt.story@axialmarket.com>
2014-02-05 11:34:34 -0500

    slight factoring, breakout docstrings v. README
    Signed-off-by: Matthew Story <matt.story@axialmarket.com>

--- betterconfig.py
+++ betterconfig.py
@@ -1,104 +1,54 @@
 #!/usr/bin/python
 '''
-============
-betterconfig
-============
-
 betterconfig provides a more convenient and extensible configuration language,
-built on python's builtin ConfigParser format.
+and a simpler built on python's builtin ConfigParser format, along with a
+drastically simplified interface for loading configs.
 
-Type Coercion Boilerplate Sucks
--------------------------------
+@version: 0.1b
+@author: Matt Story <matt.story@axial.net>
 
-ConfigParser, like many config languages, treats all values as strings,
-meaning that when you have configs like this:
+No More Type Coercion
+---------------------
 
-::
-
-    [config]
-    foo = 1
-    bar = a,list,of,strings
-    baz = just a plain old string
-
-you end up with boilerplate that looks like this:
-
-::
-
-    from ConfigParser import ConfigParser
-    MAP = {
-        'foo': int,
-        'bar': lambda x: x.split(','),
-        'baz': str,
-    }
-    c = ConfigParser()
-    c.read('./foo.cfg')
-    config = {k:MAP[k](v) for k,v in c.items('config')}
-
-Don't you really wish you could just do this:
-
-::
+betterconfig provides typing support for a variety of python literals by way
+of the ast module:
 
     [config]
     foo = 1
     bar = ['a', 'list', 'of', 'strings']
     baz = "just a plain old string"
 
-and drop the map?
+The following types are supported: strings, numbers, tuples, lists, dicts,
+booleans, and None.
 
-::
+Glob Includes
+-------------
 
-    import betterconfig
-    config = betterconfig.load('./foo.cfg')['config']
+If the 'include' directive is specified in the sectionless part of the
+configuration, betterconfig will glob expand each include and then include
+them:
 
-betterconfig supports all literal types supported by ast.literal_eval_:
-strings, numbers, tuples, lists, dicts, booleans, and None.
+    # this
+    include =  'includes/*.cfg'
+    # or this
+    include =  ['include/foo.cfg', '/etc/defaults.cfg']
 
-.. _ast.literal_eval: http://docs.python.org/2/library/ast.html#ast.literal_eval
+You can include even from included configs.
 
-More Flexibility in Config, Means Less Config by Module
--------------------------------------------------------
+Some Things to Know
+-------------------
 
-We wanted a config language that was as easy to use as a settings module in
-django or flask (and nearly as extensible), but less magical to initialize,
-and slightly safer than something like this:
+Values defined in a section with the same name as the 'default' keyword
+argument passed to 'load' (default: '_') will be made available at sectionless
+scope:
 
-::
+    [_]
+    foo = 'bar'
 
-    import importlib
-    settings = importlib.import_module('settings')
+Would yield:
 
-So we want a config that can do stuff like this:
+    betterconfig.load('./foo.cfg')['foo'] # 'bar'
 
-::
-
-    top_level  = 'variables defined outside of sections'
-    include    = ['./include.cfg', 'include.d/*.cfg']
-
-    [section]
-    namespaced = True
-
-And we don't want to have to iterate sections or items, we really just want to
-load it into a dict:
-
-::
-
-    import betterconfig
-    settings = betterconfig.load('./fancy.cfg')
-
-And if you're really in love with `.' notation, you can always do something
-silly like make a module `settings.py' that does something magical like:
-
-::
-
-    import betterconfig
-    globals().update(betterconfig.load('./fancy.cfg'))
-
-Authors
--------
-
-| Matthew Story <matt.story@axial.net>
-| Inspired By: http://stackoverflow.com/a/6209146
-| Inspired By: http://stackoverflow.com/a/2819788
 
 '''
 import os
@@ -140,18 +90,34 @@ class _Sectionless(object):
 ######## END SECTIONLESS HACKS
 
 def _id_and_dir(cfg_file):
-    '''Return dirname and inode of open filish'''
+    '''Return inode and dirname of open filish if they are available.'''
     dir_name = os.path.dirname(getattr(cfg_file, 'name', ''))
     try:
         return os.fstat(cfg_file.fileno()).st_ino, dir_name
     except AttributeError:
         return None, dir_name
 
+def _expand_includes(include, cur_dir):
+    '''Do a glob expansion on one or more included config files.
+        include
+         - one (basestr) or more (iterable) include globs to expand.
+        cur_dir
+         - cur_dir: directory to base relative include globs off of'''
+    expanded = []
+    include = [include] if isinstance(include, basestring) else include
+    for glob_includes in include:
+        # expand and sort to support 0001-style convention
+        glob_includes = glob.glob(os.path.join(cur_dir, glob_includes))
+        glob_includes.sort()
+        expanded.extend(glob_includes)
+
+    return expanded
+
 def load(*cfgs, **kwargs):
     '''Load a betterconfig config file into a python dict and return.'''
-    include = kwargs.pop('include', 'include')
-    default = kwargs.pop('default', '_')
-    seen = kwargs.pop('seen', set())
+    opts = { 'include': kwargs.pop('include', 'include'),
+             'default': kwargs.pop('default', '_'),
+             'seen': kwargs.pop('seen', set()) }
     if kwargs:
         raise TypeError('{} is an invalid keyword argument'\
                         'for this function'.format(kwargs.keys()[0]))
@@ -159,38 +125,32 @@ def load(*cfgs, **kwargs):
     compiled_config = {}
     for cfg in cfgs:
         includes = []
-        with _Sectionless(cfg, default) as cfg_file:
+        with _Sectionless(cfg, opts['default']) as cfg_file:
             # prevent infinite include recursion
             id_, cfg_dir = _id_and_dir(cfg_file)
-            if id_ in seen:
+            if id_ in opts['seen']:
                 continue
             elif id_ is not None:
-                seen.add(id_)
+                opts['seen'].add(id_)
 
             parser = ConfigParser()
             parser.readfp(cfg_file)
 
             for sect in parser.sections():
                 sect_config = compiled_config
-                if sect != default:
+                if sect != opts['default']:
                     sect_config = compiled_config.setdefault(sect, {})
 
                 for key,val in parser.items(sect):
                     val = ast.literal_eval(val)
-                    if sect == default and key == include:
-                        val = [val] if isinstance(val, basestring) else val
-                        for glob_includes in val:
-                            # expand and sort to support 0001-style convention
-                            glob_includes = glob.glob(os.path.join(cfg_dir, glob_includes))
-                            glob_includes.sort()
-                            includes.extend(glob_includes)
+                    if sect == opts['default'] and key == opts['include']:
+                        includes.extend(_expand_includes(val, cfg_dir))
                     else:
                         sect_config[key] = val
 
         # after we've finished one file, overlay any includes
         if includes:
-            compiled_config.update(load(*includes, default=default, seen=seen,
-                                        include=include))
+            compiled_config.update(load(*includes, **opts))
 
     return compiled_config
 
