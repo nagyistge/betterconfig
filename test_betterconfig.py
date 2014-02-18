233e6f8a0a6d7981f424a95d9288fe3602b245e2
Jon Rosebaugh <jon@classicspecs.com>
2014-02-17 19:18:07 -0500

    Test top-level config values.

--- test_betterconfig.py
+++ test_betterconfig.py
@@ -8,3 +8,14 @@ def test_literals():
         'baz': "just a plain old string"}}
     actual = betterconfig.load('examples/literals.cfg')
     assert expected == actual
+
+
+def test_top_level():
+    expected = {
+        'foo': {'numbers': [4, 8, 12]},
+        'config': {
+            'bar': 24
+        }
+    }
+    actual = betterconfig.load('examples/top_level.cfg')
+    assert expected == actual
