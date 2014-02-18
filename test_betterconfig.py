c1c843bfd12c867e07d05cab221caf87aa7d11b0
Jon Rosebaugh <jon@classicspecs.com>
2014-02-17 19:35:04 -0500

    Test included cfg files.

--- test_betterconfig.py
+++ test_betterconfig.py
@@ -19,3 +19,31 @@ def test_top_level():
     }
     actual = betterconfig.load('examples/top_level.cfg')
     assert expected == actual
+
+
+def test_includes():
+    expected = {
+        "foo": "Call me Ishmael",
+        "bar": {"whale": "white"},
+        "baz": 42
+    }
+    actual = betterconfig.load('examples/includes.cfg')
+    assert expected == actual
+
+
+def test_glob_includes():
+    expected = {
+        'config': {
+            'foo': 1,
+            'bar': ['a', 'list', 'of', 'strings'],
+            'baz': "just a plain old string"
+        },
+        'measurements': {'coolness': 11, 'difficulty': 0},
+        'one': {'value': 1},
+        'two': {'value': 2},
+        'sectionless': 'no section'
+    }
+    # sectionless is defined in two of the included files; the last
+    # file takes precedence
+    actual = betterconfig.load("examples/glob_includes.cfg")
+    assert expected == actual
